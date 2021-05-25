6c2d5917bd958a3c362f127c4cea7ef10274c4fb
rm <ragaamaherhanna@gmail.com>
2021-05-25 13:10:45 +0200

    Strings

--- Data_types.py
+++ Data_types.py
@@ -45,14 +45,53 @@
         my_tuple.count('p')
         my_tuple.index('l')
 
-NOTE
-Advantages of Tuple over List
-Since tuples are quite similar to lists, both of them are used in similar situations. However, there are certain advantages of implementing a tuple over a list. Below listed are some of the main advantages:
+NOTE:
+    Advantages of Tuple over List Since tuples are quite similar to lists, both of them are used in similar
+situations. However, there are certain advantages of implementing a tuple over a list. Below listed are some of the
+main advantages:
 
 We generally use tuples for heterogeneous (different) data types and lists for homogeneous (similar) data types.
 Since tuples are immutable, iterating through a tuple is faster than with list. So there is a slight performance boost.
 Tuples that contain immutable elements can be used as a key for a dictionary. With lists, this is not possible.
 If you have data that doesn't change, implementing it as tuple will guarantee that it remains write-protected.
+
+
+4- Strings:
+    ** The + operator does this in Python. Simply writing two string literals together also concatenates them.
+    ** The * operator can be used to repeat the string for a given number of times.
+    ** string using a for loop.
+    ** my_string[5] = 'a'
+    ** 'str' object does not support item assignment
+
+    str = 'cold'
+    # enumerate()
+    list_enumerate = list(enumerate(str)) --> list(enumerate(str) =  [(0, 'c'), (1, 'o'), (2, 'l'), (3, 'd')]
+
+
+    ** Python String Formatting:
+        * Escape Sequence: "https://www.w3schools.com/python/gloss_python_escape_characters.asp"
+        # using triple quotes
+        print('''He said, "What's there?"''')
+
+        # escaping single quotes
+        print('He said, "What\'s there?"')
+
+        # escaping double quotes
+        print("He said, \"What's there?\"")
+
+        *The format() Method for Formatting Strings:
+            1- Default Order
+            2- Positional Order
+            3- Keyword Order
+            4- mixed arguments
+
+        * There are numerous methods available with the string object. The format() method that we
+        mentioned above is one of them. Some of the commonly used methods are lower(), upper(), join(), split(),
+        find(), replace() etc. Here is a complete list of all the
+
+
+
+
 
 
 
